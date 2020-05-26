d9f180d811a360a0811796b04d722f8b2c87ee64
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-05-26 03:27:17 +0300

    ~ wip

--- js/js.tests/test/org/jetbrains/kotlin/js/test/BasicBoxTest.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/BasicBoxTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -920,6 +920,8 @@ abstract class BasicBoxTest(
 
         @JvmStatic
         protected val runTestInNashorn = getBoolean("kotlin.js.useNashorn")
+        @JvmStatic
+        protected val runTestInJ2V8 = getBoolean("kotlin.js.useJ2V8")
 
         val TEST_MODULE = "JS_TESTS"
         private val DEFAULT_MODULE = "main"
